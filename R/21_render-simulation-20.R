9d86f22ad9a7e6ece05d8ce14bac0819c9576a3a
Agniel, Denis <dagniel@rand.org>
2019-04-16 09:45:36 -0500

    Test run canonical sim.

---
+++ R/21_render-simulation-20.R
@@ -0,0 +1,10 @@
+library(here)
+options(
+  clustermq.defaults = list(ptn="short",
+                            log_file="Rout/log%a.log",
+                            time_amt = "2:00:00"
+  )
+)
+rmarkdown::render(here(
+  'R/20_canonical-sim.R'
+))
