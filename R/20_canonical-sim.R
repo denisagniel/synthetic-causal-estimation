1de981f1386d3f5227ae6e9a99cacfa626922434
Agniel, Denis <dagniel@rand.org>
2019-04-16 10:07:07 -0500

    Update canonical sim test.

--- R/20_canonical-sim.R
+++ R/20_canonical-sim.R
@@ -7,6 +7,54 @@ library(knitr)
 opts_chunk$set(warning = FALSE, message = FALSE, cache = FALSE, fig.width = 7, fig.height = 7)
 
 #'
+remotes::install_github('denisagniel/synthate')  
+tmpdir <- '/n/data1/hms/dbmi/zaklab/dma12/synthetic-causal-estimation/tmp'
+fs::dir_create(tmpdir)
+
+library(splines)
+  library(stringr)
+  library(purrr)
+  library(tidyr)
+  library(dplyr)
+  library(clustermq)
+  library(synthate)
+  library(grf)
+library(glue)
+#'
+#' Set up simulations settings.
+#' 
+sim_params <- expand.grid(
+  dgp = c('ks', 'ld', 'ls', 'ik', 'fi', 'iw', 'pa'),
+  j = 1:4,
+  n = c(500, 2000, 8000),
+  run = 1:3
+)
+#'  
+#' Proposed ATE list. 
+#' 
+  ate_list <- list(
+    ipw = ipw2_ate,
+    regr = regr_ate,
+    strat_regr = strat_regr_ate,
+    dr = dr_ate,
+    strat = strat_ate,
+    
+    match_ps = match_ps_ate,
+    match_prog = match_prog_ate,
+    match_both = match_both_ate,
+    cal_ps = caliper_ps_ate,
+    cal_prog = caliper_prog_ate,
+    cal_both = caliper_both_ate,
+    
+    bal = bal_ate,
+    hdbal = highdim_bal_ate
+  )
+  
+    
+  #'
+  #' Specify simulation function.
+  #' 
+sim_fn <- function(n, j, d, s, ate_list, B) {
   library(splines)
   library(stringr)
   library(purrr)
@@ -15,39 +63,14 @@ opts_chunk$set(warning = FALSE, message = FALSE, cache = FALSE, fig.width = 7, f
   library(clustermq)
   library(synthate)
   library(grf)
-#'
-#' Set up simulations settings.
-#' 
-sim_params <- expand.grid(
-  n = 
-)
-#'  
-#' Proposed ATE list. 
-#' 
-  ate_list <- list(
-    ipw = ipw2_ate,
-    strat = strat_ate,
-    strat_regr = strat_regr_ate,
-    match_ps = match_ps_ate,
-    match_prog = match_prog_ate,
-    match_both = match_both_ate,
-    cal_ps = caliper_ps_ate,
-    cal_both = caliper_both_ate,
-    grf = grf_ate,
-    regr = regr_ate,
-    dr = dr_ate,
-    bal = bal_ate,
-    hdbal = highdim_bal_ate
-  )
-  
-  
+  library(glue)
   print(
     glue::glue('Sample size is {n}; the outcome is {(j %in% c(1,3))}; the PS is {(j %in% c(1,2))}; the DGP is {d}; the seed is {s}.')
   )
   set.seed(s)
-#'
-#' Generate the data.
-#' 
+  #'
+  #' Generate the data.
+  #' 
   gen_mod <- generate_data(n = n, dgp = d, 
                            correct_outcome = (j %in% c(1,3)),
                            correct_ps = (j %in% 1:2))
@@ -58,9 +81,9 @@ sim_params <- expand.grid(
   ps_fam <- gen_mod$ps_fam
   cov_ids <- gen_mod$cov_ids
   print('data generated..')
-#'
-#' Do the estimation.
-#' 
+  #'
+  #' Do the estimation.
+  #' 
   this_data <- estimate_scores(this_data, outcome_fm = outcome_fm,
                                ps_fm = ps_fm,
                                ps_fam = ps_fam,
@@ -83,25 +106,8 @@ sim_params <- expand.grid(
   predict_delta <- function(d) {
     as.vector(predict(grf_fit, newdata = d)$predictions)
   }
-#'
-#' ## How long do the estimators take to fit?
-#' We can look at how long it takes to resample `r B` of each estimator with $n = 2000$.
-#'
-  map(ate_list, function(a) {
-    tibble(time = system.time({
-      resample_thetas <- 
-        resample_fn(dat = this_data,
-                    dpredfn = predict_delta,
-                    B = B,
-                    ate_list = list(a),
-                    outcome_fm = outcome_fm,
-                    ps_fm = ps_fm,
-                    ps_fam = ps_fam,
-                    outcome_fam = outcome_fam,
-                    cov_ids = cov_ids)
-    })[3])
-  }) %>% bind_rows(.id = 'estimator') %>%
-    kable
+  #'
+  
   resample_thetas <- resample_fn(dat = this_data,
                                  dpredfn = predict_delta,
                                  B = B,
@@ -111,18 +117,11 @@ sim_params <- expand.grid(
                                  ps_fam = ps_fam,
                                  outcome_fam = outcome_fam,
                                  cov_ids = cov_ids)
-#'
-#' It seems that the causal forest is much much slower than all others, and the balancing estimator is also quite slow. 
-#' 
   
   print('resampling done...')
   boot_theta <- resample_thetas[[1]] 
   null_theta <- resample_thetas[[2]] 
   
-#'
-#' ### Form of the object to save
-#' 
-#' You can save list columns in data frames, so we can save all of the estimators, all of the bootstrap estimates, and all of the demeaned bootstraps in their own separate columns. 
   out_df <- tibble(
     n = n,
     d = d,
@@ -133,27 +132,26 @@ sim_params <- expand.grid(
     demeaned_theta = list(null_theta)
   )
   out_df
-#'
-#' ### Working with that object
-#' 
-#' Then I've written a couple of functions to operate on that data frame. The following function accepts a model object (which stores all of the formulas and variable names for the DGP) and a vector of estimator names. Then it spits out a new function that you can apply to the data frame with all the results to get the combined estimator just for those estimators. You can also pass it additional arguments to pass to the combination function, like which estimator to set as the $\theta_0$. 
-  make_synthetic_fn <- function(gen_mod, ates = NULL, ...) {
-    
-  }
-#'
-#' Then, we can use that function to, for example, create a new function to combine regression, IPW, DR, and balancing, setting DR as $\theta_0$.
-#' 
-  my_synth_fn <- make_synthetic_fn(gen_mod, ates = c('ate_regr',
-                                                     'ate_ipw_2',
-                                                     'ate_dr',
-                                                     'ate_bal'),
-                                   name_0 = 'ate_dr'
-                                   )
-  
-  
-#'
-#' Then apply that function to the data frame like so:
-#' 
-  out_df %>%
-    mutate(theta_s = unlist(map2(thetahat, boot_theta, my_synth_fn)))
+  saveRDS(out_df, 
+          glue(tmpdir,'tmp-res_n-{n}_d-{d}_j-{j}_s-{s}.rds'))
+}  
 
+sim_res <- Q(sim_fn, 
+             j = sim_params$j,
+             n = sim_params$n,
+             d = sim_params$d,
+             s = sim_params$run,
+             const = list(
+               ate_list = ate_list,
+               B = 200),
+             n_jobs = 25
+)
+saveRDS(sim_res, 
+        here(glue('results/canonical_sim.rds'))
+)
+fs::dir_delete(tmpdir)
+sim_tib <- bind_rows(sim_res)
+saveRDS(sim_tib, 
+        here(glue('results/canonical_sim.csv'))
+)
+sessionInfo()
